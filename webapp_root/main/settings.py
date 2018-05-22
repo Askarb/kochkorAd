1f21fcd33413806c911d75530d6f3eaa016ae5a7
Askar Bolotbekov <bolotbekov06@gmail.com>
2018-05-22 12:54:58 +0600

    added django jet

--- webapp_root/main/settings.py
+++ webapp_root/main/settings.py
@@ -17,6 +17,7 @@ ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['kochkorcity.kg'])
 
 
 INSTALLED_APPS = [
+    'jet',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- webapp_root/main/urls.py
+++ webapp_root/main/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import url
 from django.contrib import admin
 from django.conf.urls.static import static
-from django.conf import settings, urls as handle_erls
+from django.conf import settings
 from django.conf.urls.i18n import i18n_patterns
 from django.urls import path, include
 
-from webapp import views
 
 urlpatterns = [
+    path('jet/', include('jet.urls', 'jet')),
     path('admin/', admin.site.urls),
     path('i18n/', include('django.conf.urls.i18n'), name='set_language')
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
@@ -15,5 +15,3 @@ urlpatterns = [
 urlpatterns += i18n_patterns(
     url(r'^', include('webapp.urls', namespace='webapp'))
 )
-
-# handle_erls.handler404 = views.BadURLView.as_view()
